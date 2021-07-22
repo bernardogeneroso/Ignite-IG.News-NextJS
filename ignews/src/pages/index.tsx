c13f478b3e09f4921f90bf8da9b6555aacb91366
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-07-22 10:14:28 +0100

    Add SASS

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import styles from '../styles/home.module.scss'
+
 const Home: React.FC = () => {
-  return <h1>Hello World</h1>
+  return <h1 className={styles.title}>Hello World</h1>
 }
 
 export default Home
