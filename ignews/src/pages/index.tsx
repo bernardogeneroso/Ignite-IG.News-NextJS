7290da5da6c58d3f41edbe98ad1f6817b52b0a58
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-07-22 09:59:17 +0100

    Add typescript in project

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
     <h1>Hello World</h1>
   )
--- ignews/src/pages/products.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Products = () => (
-  <h1>Products</h1>
-)
-
-export default Products
