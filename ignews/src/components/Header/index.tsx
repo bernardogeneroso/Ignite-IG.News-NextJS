81d4a1732dffb2f3eb68b30ee0d4f2e7f9c82887
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-07-22 10:56:35 +0100

    Component Header

---
+++ ignews/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
