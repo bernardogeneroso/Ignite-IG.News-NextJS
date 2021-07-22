ce56e6e92d616728ba0f29e506cb51b9f319a29e
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-07-22 10:39:42 +0100

    Add global styles

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 const Home: React.FC = () => {
   return (
     <>
@@ -10,7 +8,7 @@ const Home: React.FC = () => {
         <title>Home | ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   )
 }
