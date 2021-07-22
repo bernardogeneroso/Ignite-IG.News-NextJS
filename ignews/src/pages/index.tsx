c173c427abcd892c0bd542927d3649bd14cb14e2
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-07-22 10:29:33 +0100

    Add dynamic title with Head component

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Head from 'next/head'
 
 import styles from '../styles/home.module.scss'
 
 const Home: React.FC = () => {
-  return <h1 className={styles.title}>Hello World</h1>
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>Hello World</h1>
+    </>
+  )
 }
 
 export default Home
