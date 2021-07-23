ae30cc288857987f9f9c14e93646ada63314cdee
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-07-23 12:58:12 +0100

    Resolving client duplication in strip

--- ignews/src/pages/api/subscribe.ts
+++ ignews/src/pages/api/subscribe.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/client'
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../services/fauna'
 import { stripe } from '../../services/stripe'
+
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -8,12 +20,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       user: { email },
     } = await getSession({ req })
 
-    const stripeCustomer = await stripe.customers.create({
-      email,
-    })
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email)))
+    )
+
+    let customerId = user.data.stripe_customer_id
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email,
+      })
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      )
+
+      customerId = stripeCustomer.id
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [{ price: 'price_1JG2r6KAuZRKxjsNIgZsdSAw', quantity: 1 }],
