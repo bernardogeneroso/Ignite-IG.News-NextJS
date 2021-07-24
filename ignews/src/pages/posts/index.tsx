f64660b3340f209fa6ddc4025b484b881c5eb71b
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-07-24 10:55:54 +0100

    Format posts data,  and map posts data on client

--- ignews/src/pages/posts/index.tsx
+++ ignews/src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
 import { getPrismicClient } from '../../services/prismic'
 
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -15,33 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.postsList}>
-          <a href="#">
-            <time>12 de março 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ergo
-              infelix una molestia, fellx rursus, cum is ipse anulus in
-              praecordiis piscis inventus est?
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ergo
-              infelix una molestia, fellx rursus, cum is ipse anulus in
-              praecordiis piscis inventus est?
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março 2021</time>
-            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ergo
-              infelix una molestia, fellx rursus, cum is ipse anulus in
-              praecordiis piscis inventus est?
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -59,10 +51,28 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   )
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.id,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-pt',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    }
+  })
+
+  console.log(posts)
 
   return {
-    props: {},
+    props: { posts },
     //revalidate: 60 * 60 * 24, // 24 hours or 1 day
   }
 }
